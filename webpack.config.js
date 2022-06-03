ea9adbf5139b2156de117d36a67838040dc385c4
Carl Bush <CarlBushs13@gmail.com>
2022-06-03 13:58:55 -0700

    image-webpack-loader complete

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,42 @@ const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPl
 
 module.exports = {
     //The entry point is the root of the bundle and the beginning of the dependency graph
-    entry: "./assets/js/script.js",
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js",
+    },
     //takes entry and bundles code and output that code to a folder
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "main.bundle.js"
+        filename: "[name].bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                //regex to test for only .jpg files
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            esModule: false,
+                            name(file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function (url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        //used to reduce image size
+                        loader: "image-webpack-loader"
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
